d0cdaf139794c3cceffc99897ea75d4c6fafceec
Damir-Fullstack <ortukovdamir773@gmail.com>
2026-01-28 13:54:25 +0100

    Add initial script for alert and display functionality
    Initialize script with alert and display elements.

---
+++ script.js
@@ -0,0 +1,10 @@
+const cd = document.getElementById("div");
+const alert = document.getElementById("alert");
+const code = "0297";
+let con = "0000";
+alert.innerHTML = "Without Mistakes"
+cd.innerHTML = "0000";
+function del() {
+  con = "0000";
+  cd.innerHTML = con;
+}
